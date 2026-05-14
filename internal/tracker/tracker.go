1da08c1bd7a141284a27a3ac0562a58ac845862e
KodarDev <kodarov@gmail.com>
2026-05-15 00:57:54 +0300

    Error [#505564]

--- internal/tracker/tracker.go
+++ internal/tracker/tracker.go
@@ -1,6 +1,8 @@
 package tracker
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Item struct {
 	ID   string
@@ -19,12 +21,35 @@ func NewTracker() *Tracker {
 	return &Tracker{}
 }
 
-func (t *Tracker) AddItem(item Item) {
+func (t *Tracker) AddItem(item Item) (Item, error) {
+	index, exist := t.indexOf(item.ID)
+	if exist {
+		return t.items[index], ErrAlreadyExists
+	}
 	t.items = append(t.items, item)
+	return item, nil
 }
 
 func (t *Tracker) GetItems() []Item {
 	res := make([]Item, len(t.items))
 	copy(res, t.items)
 	return res
+}
+
+func (t *Tracker) UpdateItem(item Item) error {
+	index, ok := t.indexOf(item.ID)
+	if !ok {
+		return ErrNotFound
+	}
+	t.items[index] = item
+	return nil
+}
+
+func (t *Tracker) indexOf(id string) (int, bool) {
+	for i, item := range t.items {
+		if item.ID == id {
+			return i, true
+		}
+	}
+	return -1, false
 }
--- internal/tracker/tracker_test.go
+++ internal/tracker/tracker_test.go
@@ -15,7 +15,8 @@ func TestTrackerGetItems(t *testing.T) {
 
 		tr := tracker.NewTracker()
 		item := tracker.Item{ID: "1", Name: "First Item"}
-		tr.AddItem(item)
+		_, err := tr.AddItem(item)
+		assert.NoError(t, err)
 
 		res := tr.GetItems()
 		res[0].Name = "Second Item"
@@ -33,7 +34,8 @@ func TestTrackerAddItem(t *testing.T) {
 		tr := tracker.NewTracker()
 		item := tracker.Item{ID: "1", Name: "First Item"}
 
-		tr.AddItem(item)
+		_, err := tr.AddItem(item)
+		assert.NoError(t, err)
 
 		assert.Equal(t, []tracker.Item{item}, tr.GetItems())
 	})
@@ -45,9 +47,45 @@ func TestTrackerAddItem(t *testing.T) {
 		first := tracker.Item{ID: "1", Name: "First Item"}
 		second := tracker.Item{ID: "2", Name: "Second Item"}
 
-		tr.AddItem(first)
-		tr.AddItem(second)
+		_, err := tr.AddItem(first)
+		assert.NoError(t, err)
+
+		_, err = tr.AddItem(second)
+		assert.NoError(t, err)
 
 		assert.Equal(t, []tracker.Item{first, second}, tr.GetItems())
 	})
+
+	t.Run("error add - already exists", func(t *testing.T) {
+		t.Parallel()
+
+		tr := tracker.NewTracker()
+		first := tracker.Item{ID: "1", Name: "First Item"}
+		second := tracker.Item{ID: "1", Name: "Second Item"}
+
+		_, err := tr.AddItem(first)
+		assert.NoError(t, err)
+
+		_, err = tr.AddItem(second)
+		assert.ErrorIs(t, err, tracker.ErrAlreadyExists)
+		assert.Equal(t, []tracker.Item{first}, tr.GetItems())
+	})
+}
+
+func TestTrackerUpdateItem(t *testing.T) {
+	t.Parallel()
+
+	t.Run("error update - not found", func(t *testing.T) {
+		t.Parallel()
+
+		tr := tracker.NewTracker()
+		item := tracker.Item{
+			ID:   "1",
+			Name: "First Item",
+		}
+
+		err := tr.UpdateItem(item)
+		assert.ErrorIs(t, err, tracker.ErrNotFound)
+	})
+
 }
